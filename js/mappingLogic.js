9c75e6bd41c4c2fdc72d336faab2ada745a31165
SAURABH MONDAL <saurabh.jopl@gmail.com>
2025-12-27 14:00:39 +0530

    Update mappingLogic.js

--- js/mappingLogic.js
+++ js/mappingLogic.js
@@ -1,9 +1,18 @@
 export async function loadFixedCSVs() {
-  const skuMap = await fetch('./data/seller_sku_vs_uniware_sku.csv').then(r => r.text());
-  const statusMap = await fetch('./data/seller_sku_vs_status.csv').then(r => r.text());
+  const skuMapping = await fetch('./data/seller_sku_vs_uniware_sku.csv')
+    .then(res => {
+      if (!res.ok) throw new Error('SKU mapping file not found');
+      return res.text();
+    });
+
+  const skuStatus = await fetch('./data/seller_sku_vs_status.csv')
+    .then(res => {
+      if (!res.ok) throw new Error('SKU status file not found');
+      return res.text();
+    });
 
   return {
-    skuMap,
-    statusMap
+    skuMapping,
+    skuStatus
   };
 }
