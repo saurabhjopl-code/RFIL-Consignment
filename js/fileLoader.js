2fa61c38a81cb321f9e885452dc1a668d88fd33e
SAURABH MONDAL <saurabh.jopl@gmail.com>
2025-12-27 14:02:40 +0530

    Update fileLoader.js

--- js/fileLoader.js
+++ js/fileLoader.js
@@ -1,10 +1,8 @@
 export function loadFile(file) {
   return new Promise((resolve, reject) => {
     const reader = new FileReader();
-
     reader.onload = e => resolve(e.target.result);
     reader.onerror = err => reject(err);
-
     reader.readAsBinaryString(file);
   });
 }
